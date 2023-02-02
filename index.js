4e01e9dfada4bc69b044c3eeda171040416f4524
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-02 10:07:01 -0600

    Game finished, can whack a mole!

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 let score = 0;
+
+const holes = [...document.getElementsByClassName('hole')];
+
+function generateRandomMole() {
+  const randomHoleIndex = Math.floor(Math.random() * holes.length);
+  holes[randomHoleIndex].classList.toggle("mole");
+}
+
+
+setInterval(generateRandomMole, 300)
+
+const scoreDisplay = document.getElementById('score');
+
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent) {
+  if (clickEvent.target.matches('.mole')) {
+    clickEvent.target.classList.remove('mole');
+    score += 1;
+    scoreDisplay.innerText = score;
+  }
+});
